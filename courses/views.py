c22517dcc2b8278817a89272ffd08bb0b3febbbf
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-01-16 16:57:27 +0700

    menambahkan details view

--- courses/views.py
+++ courses/views.py
@@ -5,7 +5,10 @@ from .models import Course
 
 def course_list(request):
     courses = Course.objects.all()
-    #output = ', '.join([str(course) for course in courses])
-    #return HttpResponse("<h2>"+output+"<h2>")
     return render(request,'courses/course_list.html',{'courses':courses})
+
+def course_detail(request,pk):
+    course = Course.objects.get(pk=pk)
+    return render(request,'courses/course_detail.html',{'course':course})
+
 

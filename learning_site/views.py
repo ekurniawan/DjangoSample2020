a65f449a77d8eb84d96a681a79f9338bc3282864
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-01-16 13:42:31 +0700

    menambahkan folder templates di root

--- learning_site/views.py
+++ learning_site/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def hello_world(request):
-    return HttpResponse("<h2>Hello World</h2>")
+    return render(request,'home.html')
 
 
